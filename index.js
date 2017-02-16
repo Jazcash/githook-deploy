ff176ef8af11beac0e2e71a2981e2b41e2329ff9
jazcash <jaspercashmore@gmail.com>
2017-02-16 15:16:47 +0000

    Changed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function(req, res){
+app.all("/", function(req, res){
 	res.send("You must specify a deployment route")
 });
 
-app.get('/jazcss', function (req, res) {
+app.all('/jazcss', function (req, res) {
 	let output = "";
 	const execSync = require("child_process").execSync;
 	var gitOutput = execSync("git --git-dir=/var/www/jazcss/.git --work-tree=/var/www/jazcss pull", {
